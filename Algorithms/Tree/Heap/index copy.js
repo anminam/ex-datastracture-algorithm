09b62cead18f6ef3ca9243315a53bc74713e3f83
anminam <minamhunmin@gmail.com>
2020-09-29 11:10:03 +0900

    힙

--- Algorithms/Tree/Heap/index copy.js
+++ Algorithms/Tree/Heap/index copy.js
@@ -15,9 +15,9 @@ const heapify = (arr, length, i) => {
     heapify(arr, length, root);
   }
 };
+
 const start = (arr) => {
   const length = arr.length;
-
   for (let i = Math.floor(length / 2 - 1); i >= 0; i--) {
     heapify(arr, length, i);
   }
@@ -27,13 +27,11 @@ const start = (arr) => {
     heapify(arr, i, 0);
   }
 };
-
 const swap = (arr, i, j) => {
   const temp = arr[i];
   arr[i] = arr[j];
   arr[j] = temp;
 };
-
 const arr = [7, 6, 5, 8, 3, 5, 9, 1, 6];
 start(arr);
 console.log(arr);
